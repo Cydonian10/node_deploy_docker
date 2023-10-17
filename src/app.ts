9b4c38fe20c25026ed333f5c6f14bb8c4cadc001
Gabriel <cydonian_07@hotmail.com>
2023-10-17 10:43:21 -0500

    ssl reject false

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,9 @@ async function main() {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
   } catch (error) {
     console.log(error);

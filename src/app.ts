4b50c2ef47ca64ffe70f8ec012f3d331d5cb0c05
Gabriel <cydonian_07@hotmail.com>
2023-10-17 10:39:57 -0500

    ssl en true

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ async function main() {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: true,
     });
   } catch (error) {
     console.log(error);
